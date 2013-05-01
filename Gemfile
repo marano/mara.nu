9be4fd2383cb83e898b123ca019922b74731142e
marano <thiagomarano@gmail.com>
2013-05-01 14:09:58 +0800

    Serve less with sinatra-assetpack.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org/'
 
 gem 'sinatra'
 gem 'slim'
+gem 'sinatra-assetpack'
 gem 'therubyracer' # required by less
 gem 'less'
 gem 'rollin'
--- controller.rb
+++ controller.rb
@@ -1,12 +1,23 @@
 class Controller < Sinatra::Base
+
+  register Sinatra::AssetPack
+
+  assets {
+    Less.paths << 'views/style/bootstrap'
+
+    serve '/style', from: 'views/style'
+
+    css :application, '/style/application.css', [
+      '/style/bootstrap/bootstrap.css'
+    ]
+
+    prebuild true
+    css_compression :less
+  }
+
+
   get '/' do
     @articles = Rollin::Blog.new.articles
     slim :home
-  end
-
-  get '/*.css' do
-    css_file = params[:splat].first
-    directory = "views/#{File.dirname(css_file)}"
-    less css_file.to_sym, :paths => [ directory ]
   end
 end
