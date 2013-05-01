c7cdd27f4b413f77172b17c8a1f357b5db2ac83a
marano <thiagomarano@gmail.com>
2013-05-01 15:26:35 +0800

    Title takes user to home. Custom styling file. Home display articles titles and takes to article page on click.

--- controller.rb
+++ controller.rb
@@ -3,21 +3,37 @@ class Controller < Sinatra::Base
   register Sinatra::AssetPack
 
   assets {
+    Less.paths << 'views/style'
     Less.paths << 'views/style/bootstrap'
 
     serve '/style', from: 'views/style'
 
     css :application, '/style/application.css', [
-      '/style/bootstrap/bootstrap.css'
+      '/style/bootstrap/bootstrap.css',
+      '/style/my.css'
     ]
 
     prebuild true
     css_compression :less
   }
 
+  def blog
+    Rollin::Blog.new(articles_folder: 'articles')
+  end
 
   get '/' do
-    @articles = Rollin::Blog.new.articles
+    @articles = blog.articles
     slim :home
+  end
+
+  get '/article/:id' do
+    @article = blog.find_article_by_id(params[:id])
+    slim :article
+  end
+end
+
+class Rollin::Article
+  def link_to_self
+    "/article/#{id}"
   end
 end
