d6cf3b1dd596aa55e787e2fbaf98c171ddb97dde
marano <thiagomarano@gmail.com>
2013-05-01 13:36:57 +0800

    Compiles less and serves css files.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org/'
 
 gem 'sinatra'
 gem 'slim'
+gem 'therubyracer' # required by less
+gem 'less'
 gem 'rollin'
 
 group :development do
--- controller.rb
+++ controller.rb
@@ -3,4 +3,10 @@ class Controller < Sinatra::Base
     @articles = Rollin::Blog.new.articles
     slim :home
   end
+
+  get '/*.css' do
+    css_file = params[:splat].first
+    directory = "views/#{File.dirname(css_file)}"
+    less css_file.to_sym, :paths => [ directory ]
+  end
 end
